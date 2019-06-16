7f664d29e06a451dc64a0d773ff239bd23c3a770
J Reus <hello@jonathanreus.com>
2019-06-16 14:10:48 +0200

    added allowed origin on ws server for github address

--- code/sonification/experiments/websockets/server.py
+++ code/sonification/experiments/websockets/server.py
@@ -17,7 +17,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     server_out = ["127.0.0.1", 57120]
 
     def check_origin(self, origin):
-        allowed = ["http://localhost/", "http://127.0.0.1", "null", "file://"];
+        allowed = ["http://localhost", "http://127.0.0.1", "https://anatomiesofintelligence.github.io", "null", "file://"];
         if origin in allowed:
             print("MSG ALLOWED, from origin", origin)
             return 1
