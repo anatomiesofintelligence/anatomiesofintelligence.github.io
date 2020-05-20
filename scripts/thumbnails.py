acfc4d8c8d766b7afe277b2042cc20b3ad71abae
J Reus <info@jonathanreus.com>
2020-05-20 12:19:56 +0200

    created mid resolution 800px images

--- scripts/thumbnails.py
+++ scripts/thumbnails.py
@@ -20,12 +20,16 @@ if __name__ == "__main__":
     parser.add_argument("-twidth", type=int, default=128, help="Thumbnail width")
     parser.add_argument("-theight", type=int, default=128, help="Thumbnail height")
     parser.add_argument("-postfix", default="_th", help="Postfix for thumbnail files")
+    parser.add_argument("-ignore-postfix", default="_th,_mid", help="Ignore images with these postfixes")
     clargs = parser.parse_args()
 
     indir = os.path.abspath(clargs.inputdir)
     thumbsize = (clargs.twidth, clargs.theight)
     overwrite = clargs.overwrite
     postfix = clargs.postfix
+    ignore_postfix = clargs.ignore_postfix.split(',');
+
+    print(ignore_postfix);
 
     if not os.path.isdir(indir):
         raise IOError("Source must be a directory: {}".format(indir))
@@ -36,7 +40,11 @@ if __name__ == "__main__":
                 sourcefile = os.path.join(root, filename)
                 fn, ext = os.path.splitext(filename)
                 if ext.lower() in ['.jpeg', '.jpg', '.png', '.gif']:
+                    isthumb = False
                     isthumb = fn[(-len(postfix)):] == postfix
+                    for pf in ignore_postfix:
+                        if fn[(-len(pf)):] == pf:
+                            isthumb = True
                     if not isthumb:
                         newfilename = fn+postfix+ext
                         newfilepath = os.path.join(root, newfilename)
@@ -50,5 +58,7 @@ if __name__ == "__main__":
                         else:
                             print("Writing {}".format(newfilename))
                             makeThumb(sourcefile, newfilepath, thumbsize)
+                    else:
+                        print("Ignoring filename {}".format(filename))
                 else:
                     print("Ignoring {}".format(sourcefile))
