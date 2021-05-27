822b6081e253c0c51170e8e30b8079b2091ec247
J Reus <info@jonathanreus.com>
2021-05-27 18:03:16 +0200

    lots of updates

---
+++ collaborate.js
@@ -0,0 +1,21 @@
+// Hey Jo, are you out there?
+
+let myvar = 3;
+console.log(myvar);
+
+function blahblah() {
+  console.log("OMG! HEY I SEE YOUR CURSOR!");
+}
+
+
+function hello_Jon (){
+
+}
+
+
+
+
+
+setInterval(1000, hello_Jon());
+
+// :-D :-D :-D
